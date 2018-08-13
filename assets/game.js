e3d648c2579b69ff67cab9e9251079e2bd2ee4a6
Amanda Kramer <ajkramer1013@gmail.com>
2018-08-13 16:14:45 -0400

    change

--- assets/game.js
+++ assets/game.js
@@ -1,13 +1,23 @@
 
+
+
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-var word ;
-var guess ;
-var guesses = [];
-var lives ;
-var counter ;
+var computerWords = ["Paterno", "Quarterback", "Franklin","Linebacker"];
+var chosenWord = 3;
+var chosenIndex = null;
+var userGuess = {alphabet};
+var letterGuess = [];
+var wins = "";
+var loses = "";
+var lives = 4;
+var counter = 12 ;
 
-var showLives =
-document.getElementById("My lives");
+var computerWords = function () {
+chosenIndez = computerWords[Math.floor(Math.random() * computerWords.length)];
+console.log (chosenIndex);
+};
 
 
+document.onkeyup = function(event) {
+var letter = string.
