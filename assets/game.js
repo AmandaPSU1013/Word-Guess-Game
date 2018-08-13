1c710c9c607642ea85da2d283d07ff47b4d81407
Amanda Kramer <ajkramer1013@gmail.com>
2018-08-13 15:02:40 -0400

    change

---
+++ assets/game.js
@@ -0,0 +1,13 @@
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var word ;
+var guess ;
+var guesses = [];
+var lives ;
+var counter ;
+
+var showLives =
+document.getElementById("My lives");
+
+
